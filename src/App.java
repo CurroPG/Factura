425d62bb785b11baae5daf9d91d54738b40678fe
Curro Portillo <211curroportillo@gmail.com>
2025-09-25 11:15:48 +0200

    cambio 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Rojo Polo Paella Inc.");
+        System.out.println("Carrertera Muelle 38");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-30s%-30s%-30s%30s%n", "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s%n", "","","FECHA VCTO", "29-09-2019");
+        System.out.println("──────────────────────────────────────────────────────────────────────────");
+
+
+
     }
 }
